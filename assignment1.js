819ca565072253704a4d4adc76e804214b985e69
Rodney Sellers <quehegan@gmail.com>
2014-01-23 12:03:50 -0500

    have dots displaying correctly

--- assignment1.js
+++ assignment1.js
@@ -1,44 +1,67 @@
+var colSel;
 var ans = [];
-var selectedColor
+var numAns = 12;
+var userAns = [];
+var selectedColor;
 var numColors = 6;
 pegs  = ['redball.gif',    'blueball.gif',    'brownball.gif',
                 'greenball.gif',  'yellowball.gif',  'pinkball.gif' ];               
-                
+
+//Displays the guess pegs                
 function displayPegs(){
-	var retVal = "";
+	//var retVal = "";
 	for (color in pegs){
-		retVal += "<td align='center'>";
-		retVal += "<a href='javascript:selectColor("+ color +")'>";
-		retVal += "<img src=" + pegs[color] + " >";
-		retVal += "</a>";
-		retVal += "</td>";
+		document.write("	<td align='center'>");
+		document.write("		<a href='javascript:selectColor("+ color +")'>");
+		document.write("			<img src=" + pegs[color] + " >");
+		document.write("		</a>");
+		document.write("	</td>");
 	}
-    return retVal;
+    //return retVal;
 }
+
+//sets the current selected color to the colSel variable
 function selectColor(color){
-	alert("here");
+	colSel = color;
+	//alert(colSel);
 }            
+//sets the color
+function setColor(id){
+	//alert(id);
+	//alert(pegs[colSel]);
+	document.getElementById(id).innerHTML = "<img src="+pegs[colSel]+" border=0/>";
+}
+//Submits the guess if all pegs have been placed
 function submitGuess(){
 	alert("here guess");
-	/*
-	for(color in pegs){
-		
-	}
-	for (var c=0; c<nColumns; c++){
-            getColumnNImage(c).src = colorImages[hiddenAnswer[c]];
-        }
-    */
 }
+
+//Creates the answer that the user is trying to guess
 function createAns(){
 	for(var i=0;i<pegs.length;i++){
 		ans[i] = Math.floor( pegs.length * Math.random() );
 	}
 }
+//makes the board
+function makeBoard(){
+	for(var i=0;i<numAns;i++){
+		document.write("<tr>");
+		for(var j=0;j<numColors;j++){
+			document.write("<td id='col"+j+"row"+i+"' valign='top' align='center' onclick='setColor(this.id)'>");
+			document.write("<img src=whitepixel.gif >");
+			document.write("</a>");
+			document.write("</td>");
+		}
+		document.write("</tr>");
+	}
+}
+
+//Displays the answer
 function displayAns(){
 	var retVal = ""
 	for(color in ans){
-		retVal += "<td align='center'>";
-		retVal += "<img src=" + pegs[ans[color]] + " >";
+		retVal += "<td valign='top' style='height:37	'>";
+		retVal += "			<img src=" + pegs[ans[color]] + " >";
 		retVal += "</td>";
 	}
 	return retVal;
