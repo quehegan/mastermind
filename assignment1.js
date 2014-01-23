7dfe4f80c6af1cdfaba2add94bf3e32064cf08f7
Rodney Sellers <quehegan@gmail.com>
2014-01-23 12:38:42 -0500

    updated to use document.write

--- assignment1.js
+++ assignment1.js
@@ -4,12 +4,12 @@ var numAns = 12;
 var userAns = [];
 var selectedColor;
 var numColors = 6;
+var curGuess = 0;
 pegs  = ['redball.gif',    'blueball.gif',    'brownball.gif',
                 'greenball.gif',  'yellowball.gif',  'pinkball.gif' ];               
 
 //Displays the guess pegs                
 function displayPegs(){
-	//var retVal = "";
 	for (color in pegs){
 		document.write("	<td align='center'>");
 		document.write("		<a href='javascript:selectColor("+ color +")'>");
@@ -17,19 +17,20 @@ function displayPegs(){
 		document.write("		</a>");
 		document.write("	</td>");
 	}
-    //return retVal;
 }
 
 //sets the current selected color to the colSel variable
 function selectColor(color){
 	colSel = color;
-	//alert(colSel);
 }            
 //sets the color
-function setColor(id){
-	//alert(id);
-	//alert(pegs[colSel]);
-	document.getElementById(id).innerHTML = "<img src="+pegs[colSel]+" border=0/>";
+function setColor(id, row){
+	if(curGuess != row){
+		alert("You are on guess "+(curGuess+1)+".");
+	}
+	else{
+		document.getElementById(id).innerHTML = "<img src="+pegs[colSel]+" border=0/>";
+	}
 }
 //Submits the guess if all pegs have been placed
 function submitGuess(){
@@ -38,16 +39,17 @@ function submitGuess(){
 
 //Creates the answer that the user is trying to guess
 function createAns(){
-	for(var i=0;i<pegs.length;i++){
+	for(var i=0;i<4;i++){
 		ans[i] = Math.floor( pegs.length * Math.random() );
 	}
 }
 //makes the board
 function makeBoard(){
 	for(var i=0;i<numAns;i++){
-		document.write("<tr>");
-		for(var j=0;j<numColors;j++){
-			document.write("<td id='col"+j+"row"+i+"' valign='top' align='center' onclick='setColor(this.id)'>");
+		document.write("<tr id="+i+">");
+		document.write("<td>"+(i+1)+"</td>");
+		for(var j=0;j<4;j++){
+			document.write("<td id='col"+j+"row"+i+"' valign='top' align='center' onclick='setColor(this.id, "+i+")'>");
 			document.write("<img src=whitepixel.gif >");
 			document.write("</a>");
 			document.write("</td>");
@@ -58,11 +60,9 @@ function makeBoard(){
 
 //Displays the answer
 function displayAns(){
-	var retVal = ""
 	for(color in ans){
-		retVal += "<td valign='top' style='height:37	'>";
-		retVal += "			<img src=" + pegs[ans[color]] + " >";
-		retVal += "</td>";
+		document.write("<td valign='top' style='height:37	'>");
+		document.write("			<img src=" + pegs[ans[color]] + " >");
+		document.write("</td>");
 	}
-	return retVal;
 }
